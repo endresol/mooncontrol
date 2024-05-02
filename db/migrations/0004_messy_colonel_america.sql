2e6451cc9af36e940ef33927ca2e043cc05451d1
Endre Solem <esolem@gmail.com>
2024-05-02 21:35:07 +0200

    dropped migration

--- db/migrations/0004_messy_colonel_america.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `nfts_apenft` (
-	`id` bigint AUTO_INCREMENT,
-	`nft_id` int,
-	`address` varchar(256)
-);
---> statement-breakpoint
-CREATE INDEX `address_idx` ON `nfts_apenft` (`address`);
